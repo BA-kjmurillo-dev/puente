cfd76a880fe20b3ef2178a5424cc947df8c35e88
Jose Arturo Ochoa Javier <jochoa@bancatlan.hn>
2024-06-07 17:19:32 -0600

    Se realizaron mejoras al algoritmo de validacion de remesadora, se cambio la respuesta de null a "error"

--- src/main/java/com/puente/web/config/SecurityConfig.java
+++ src/main/java/com/puente/web/config/SecurityConfig.java
@@ -5,18 +5,16 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
 @Configuration
 public class SecurityConfig {
@@ -30,43 +28,32 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .csrf((csrfz)->csrfz
-                        .disable())
-                .cors(cors->cors
-                        .disable())
+                .csrf(AbstractHttpConfigurer::disable)
+                .cors(AbstractHttpConfigurer::disable)
                 .sessionManagement(session -> session
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 )
                 .authorizeHttpRequests((authz)->authz
                                 .requestMatchers("/api/auth/**").permitAll()
+                                .requestMatchers("/consulta/remesa").permitAll()
                                 .requestMatchers(HttpMethod.GET, "/valores/**").permitAll()
                                 .requestMatchers(HttpMethod.GET, "/api/**").hasAnyRole("ADMIN", "CUSTOMER")
-                                .requestMatchers(HttpMethod.GET, "/log").hasRole("ADMIN")
+                                .requestMatchers(HttpMethod.POST, "/api/**").hasAnyRole("ADMIN", "CUSTOMER")
+                                .requestMatchers(HttpMethod.PUT, "/api/**").denyAll()
+                                .requestMatchers(HttpMethod.DELETE, "/api/**").denyAll()
+                                //.requestMatchers(HttpMethod.GET).permitAll()
+                                //.requestMatchers(HttpMethod.POST).permitAll()
                                 //.requestMatchers(HttpMethod.PUT).denyAll()
+                                //.requestMatchers(HttpMethod.DELETE).denyAll()
                                 //.requestMatchers("/log").hasAuthority("permise_special")
                                 //.requestMatchers(HttpMethod.PUT).hasRole("ADMIN")
                         .anyRequest()
                         .authenticated()
                         )
-                //.httpBasic(Customizer.withDefaults());
                 .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
         return http.build();
     }
 
-//    @Bean
-//    public UserDetailsService memoryUsers(){
-//        UserDetails admin = User.builder()
-//                .username("admin")
-//                .password(passwordEncoder().encode("admin"))
-//                .roles("ADMIN").
-//                build();
-//        UserDetails customer = User.builder()
-//                .username("customer")
-//                .password(passwordEncoder().encode("customer123"))
-//                .roles("CUSTOMER").
-//                build();
-//        return new InMemoryUserDetailsManager(admin, customer);
-//    }
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
         return configuration.getAuthenticationManager();
--- src/main/java/com/puente/web/controller/ConsultaController.java
+++ src/main/java/com/puente/web/controller/ConsultaController.java
@@ -7,13 +7,15 @@ import com.puente.service.ConsultaServices;
 import com.puente.service.dto.Wsdl04Dto;
 import com.puente.service.dto.Wsdl07Dto;
 import com.soap.wsdl.service07.ServicesRequest007ItemSolicitud;
-import com.soap.wsdl.service07.ServicesResponse007;
+import com.puente.service.ConsultaRemesadoraService;
 import lombok.ToString;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.http.HttpStatus;
+
 
 @ToString
 @RestController
@@ -23,6 +25,8 @@ public class ConsultaController {
     private final Wsdl03Service wsdl03Service;
     private final Wsdl04Service wsdl04Service;
     private final Wsdl07Service wsdl07Service;
+    @Autowired
+     private ConsultaRemesadoraService consultaRemesadoraService;
 
     @Autowired
     public ConsultaController(
@@ -38,19 +42,33 @@ public class ConsultaController {
     }
 
     private static final Logger log = LoggerFactory.getLogger(ConsultaController.class);
-    @GetMapping("/{remesa}")
-    public String  ConsultaController(@PathVariable String remesa) {
+    @PostMapping("/remesa")
+    public ResponseEntity<String>ConsulaRenesadora(@RequestBody String remesa) {
         if (remesa == null){
             log.error("Remesa no puede ser null");
             throw new IllegalArgumentException("Remesa no puede ser null");
         }
+        remesa = remesa.replaceAll("\"", "");
+        remesa = remesa.replaceAll("\\s+", "");
         log.info("Constoller remesa:"+remesa);
-        return this.consultaServices.ConsultaRemesadora(remesa);
+        String respuesta = this.consultaServices.ConsultaRemesadora(remesa);
+        if (respuesta.equals("000004") ||
+                respuesta.equals("000007") ||
+                respuesta.equals("000016") ||
+                respuesta.equals("000018")){
+
+            return ResponseEntity.ok("Remesa: "+remesa+" Remesadora: "+respuesta);
+        } else if (respuesta.equals("000006")) {
+            return ResponseEntity.ok("Remesa: "+remesa+" Remesadora: "+respuesta + " Redireccion a SIREMU");
+        } else {
+            return ResponseEntity.status(400).body("Error");
+        }
     }
     @GetMapping("/wsdl03Test")
     public ResponseEntity<String> wsdl03Test() {
         return ResponseEntity.ok(this.wsdl03Service.getMessage());
     }
+
 
     @GetMapping("/wsdl04Test")
     public ResponseEntity<Wsdl04Dto> wsdl04Test() {
@@ -63,4 +81,5 @@ public class ConsultaController {
     ) {
         return ResponseEntity.ok(this.wsdl07Service.getRemittanceByIdentifier(itemSolicitudRequest));
     }
+
 }
