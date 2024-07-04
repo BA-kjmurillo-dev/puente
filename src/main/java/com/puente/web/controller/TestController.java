256be54248d73863d8dba8a58c892d66ec71e3ad
Kelvin Galo <kjmurillo@bancataln.hn>
2024-07-04 11:05:44 -0600

    Consumo Crear BP y Swagger
    Se finalizo el consumo del crea BP y se agrego la documentacion con Swagger

--- src/main/java/com/puente/web/controller/TestController.java
+++ src/main/java/com/puente/web/controller/TestController.java
@@ -6,6 +6,7 @@ import com.puente.persistence.entity.ValoresGlobalesRemesasEntity;
 import com.puente.persistence.repository.ParametroRemesadoraRepository;
 import com.puente.persistence.repository.ValoresGlobalesRemesasRepository;
 import com.puente.service.dto.*;
+import com.puente.web.exception.RequestException;
 import com.soap.wsdl.service03.SDTServicioVentanillaIn;
 import com.soap.wsdl.service03.SDTServicioVentanillaInItemRemesa;
 import com.soap.wsdl.service07.ServicesRequest007ItemSolicitud;
@@ -18,6 +19,7 @@ import org.springframework.web.bind.annotation.*;
 import com.puente.service.*;
 import com.puente.service.dto.ComparacionNombreDto;
 
+import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -169,8 +171,55 @@ public class TestController {
         return ResponseEntity.ok(list);
     }
 
+    /**
+     * @description Metodo Post que se utiliza para el llamado del servicio SOAP para la creacion del BP.
+     * @param datosBpDto Recibe un objeto de tipo DatosBpDto, el cual contiene la información mínima para la creacion
+     *                   de un BP.
+     * @return DTCreaBusinessPartnerResp Retorna un objeto de tipo DTCreaBusinessPartnerResp, el cual contiene la
+     *                                   información de la creación del BP.
+     * */
     @PostMapping("/wsdlbp")
     public ResponseEntity<?> wsdlBpTestCreacion(@RequestBody DatosBpDto datosBpDto){
+        //Obtenemos la clase de nuestro objeto
+        Class<?> miClase = datosBpDto.getClass();
+
+        //Obtenemos los nombres de los atributos de nuestra clase
+        Field[] campos = miClase.getDeclaredFields();
+
+        //Validamos que el cuerpo de la petición este completo
+        if (datosBpDto != null){
+            // Recorremos los atributos del objeto que viene de la petición para realizar las validaciones
+            for (Field campo : campos){
+                //Habilitamos los atributos que sean privados para poder acceder a ellos
+                campo.setAccessible(true);
+
+                //Variable que servirá para guardar el contenido de cada atributo. Para ello haremos un Casteo.
+                String valorCampoCasteado = "";
+
+                //Obtenemos el valor del atributo
+                try {
+                    //Obtenemos el nombre del atributo
+                    String nombreCampo = campo.getName();
+
+                    if (campo.get(datosBpDto) instanceof String){
+                        valorCampoCasteado = (String) campo.get(datosBpDto);
+                    }
+
+                    if (campo.get(datosBpDto) != null){
+                        if (valorCampoCasteado.isEmpty() || valorCampoCasteado.isBlank()){
+                            throw new RequestException("E-P003", "El contenido de la etiqueta [" + nombreCampo + "] no puede ser vacío.");
+                        }
+                    } else{
+                        throw new RequestException("E-P002", "Falta la etiqueta [" + nombreCampo + "] dentro del cuerpo de la petición");
+                    }
+
+                } catch (IllegalAccessException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }else {
+            throw new RequestException("E-P001", "El cuerpo de la petición no puede estar vacío.");
+        }
         return ResponseEntity.ok(wsdlBpClient.getResponseCreateBp(datosBpDto));
     }
 }
